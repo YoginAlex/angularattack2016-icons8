c814de0a54ba9f7d8fef8e3079cabf0aee956d1e
Alexey Zakharenko <yogin.alex@gmail.com>
2016-05-14 07:22:50 +0300

    [remove] unused components and routing

--- src/client/app/components/app.component.ts
+++ src/client/app/components/app.component.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES, Routes } from '@angular/router';
 
-import { AboutComponent } from '../+about/index';
 import { HomeComponent } from '../+home/index';
 import { NameListService } from '../shared/index';
-import { NavbarComponent } from './navbar.component';
-import { ToolbarComponent } from './toolbar.component';
 
 @Component({
   selector: 'sd-app',
   viewProviders: [NameListService],
   templateUrl: 'app/components/app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [ROUTER_DIRECTIVES, HomeComponent],
+  styleUrls: ['app/components/app.component.css']
 })
-@Routes([
-  {
-    path: '/',
-    component: HomeComponent
-  },
-  {
-    path: '/about',
-    component: AboutComponent
-  }
-])
 export class AppComponent {}
