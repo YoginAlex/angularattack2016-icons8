d5bfa02bd48a51f24b5cc49c344386c3c8f978ce
Nikita Hristinin <nkhristinin@gmail.com>
2016-05-14 18:12:23 +1000

    add Access-Control-Allow-Origin

--- src/server/main.js
+++ src/server/main.js
@@ -14,6 +14,12 @@ var port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var router = express.Router();
 
 var visual_recognition = watson.visual_recognition({
