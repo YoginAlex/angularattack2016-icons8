ff9f6b5dc6b5e70ef3c91ca4f50223cacf9bc423
Nikita Hristinin <nkhristinin@gmail.com>
2016-05-15 17:08:53 +1000

    method person for info sikin type

--- src/client/app/shared/person.ts
+++ src/client/app/shared/person.ts
@@ -1,8 +1,8 @@
 import {Score} from "./scores";
 export enum SEX {
-  WOMEN,
-  MEN,
-  OTHER
+  women,
+  men,
+  other
 }
 
 export enum SKIN_TYPE{
@@ -24,34 +24,88 @@ export class Person {
     var maxSkinTypeScore;
     var topSkinType;
 
+    var maxSexScore;
+    var topSex;
+
     var skinTypes = scores.forEach(item=> {
       if (item.id.indexOf('Type') > -1) {
         if (!maxSkinTypeScore || item.score > maxSkinTypeScore) {
           maxSkinTypeScore = item.score;
           topSkinType = SKIN_TYPE[item.id.split("_")[0].toLowerCase()];
-
         }
       }
 
-      if (item.id.indexOf('gender_sex_men') > -1) {
-        if (item.score > 0.5) {
-          this.sex = SEX.MEN;
-        } else {
-          this.sex = SEX.WOMEN;
+      if (item.id.indexOf('gender_') > -1) {
+        if (!maxSexScore || item.score > maxSexScore) {
+          maxSexScore = item.score;
+          topSex = SEX[item.id.split("_")[1].toLowerCase()];
         }
       }
     });
 
-    if (!topSkinType) {
+    if (topSkinType != undefined) {
       this.skinType = topSkinType;
     } else {
       this.skinType = SKIN_TYPE.other
     }
 
-    if (!this.sex) {
-      this.sex = SEX.OTHER;
+    if (topSex != undefined) {
+      this.sex = topSex;
+    } else {
+      this.sex = SEX.other
     }
 
+    debugger;
+
+
+  }
+
+  type() {
+    let string = 'Fitzpatrick type ';
+    let skinTypeInfo = {
+      type1: {
+        number: 'I',
+        info: 'pale white; blond or red hair; blue eyes; freckles'
+      },
+      type2: {
+        number: 'II',
+        info: 'white; fair; blond or red hair; blue, green, or hazel eyes'
+      },
+      type3: {
+        number: 'III',
+        info: 'cream white; fair with any hair or eye color'
+      },
+      type4: {
+        number: 'IV',
+        info: 'moderate brown'
+      },
+      type5: {
+        number: 'V',
+        info: 'dark brown'
+      },
+      type6: {
+        number: 'VI',
+        info: 'deeply pigmented dark brown to darkest brown'
+      }
+    };
+
+    let type = skinTypeInfo[SKIN_TYPE[this.skinType]];
+    if (!type) {
+      return false;
+    } else {
+      return string + type.number + ' (' + type.info + ')';
+    }
+
+  }
+
+  sexToSting() {
+    if (this.sex == SEX.women) {
+      return 'Female';
+    } else if (this.sex == SEX.men) {
+      return 'Male';
+    } else {
+      return false;
+    }
   }
 
 }
